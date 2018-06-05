a241341ad25eb2485e3842086369ca977f52821f
vlad <tgkfdjk@gmail.com>
2018-06-05 12:52:24 +0100

    Add code push and link it

--- android/app/src/main/java/com/bossstaffapp/MainApplication.java
+++ android/app/src/main/java/com/bossstaffapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.bossstaffapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -22,6 +23,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -31,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new SplashScreenReactPackage(),
             new OrientationPackage(),
             new VectorIconsPackage(),
