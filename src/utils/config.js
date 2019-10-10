10e1ac209538e2948ef871db399ea3c515623930
William Billington <william.billington@gmail.com>
2019-10-10 15:31:39 +0200

    Take APP_LAMBDA_BASE_URL from env

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,8 @@
+import { APP_LAMBDA_BASE_URL } from 'react-native-dotenv';
+
 export default {
-  BASE: 'https://boss.jsmbars.co.uk',
-  IMAGE_BASE: 'https://boss.jsmbars.co.uk',
+  BASE: APP_LAMBDA_BASE_URL,
+  IMAGE_BASE: APP_LAMBDA_BASE_URL,
   CONFIRM_PAYMENT(paymentId) {
     return `/api/staff-app/v1/payments/${paymentId}/receive_payment`;
   },
